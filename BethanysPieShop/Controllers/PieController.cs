91f0ca0b344f0bce3660376aa9ce37f91b2538b5
João Rubio <jrubio@alter-solutions.com>
2022-08-19 17:31:51 +0200

    Modifications

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,4 +1,5 @@
 ﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
@@ -16,7 +17,10 @@ namespace BethanysPieShop.Controllers
 
         public IActionResult List()
         {
-            return View(_pieRepository.AllPies);
+            //ViewBag.CurrentCategory = "Cheese cakes";
+            //return View(_pieRepository.AllPies);
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+            return View(pieListViewModel);
         }
     }
 }
---
+++ BethanysPieShop/ViewModels/PieListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShop.Models;
+
+namespace BethanysPieShop.ViewModels
+{
+    public class PieListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; }
+        public string? CurrentCatergory { get; }
+
+        public PieListViewModel(IEnumerable<Pie> pies, string? currentCatergory)
+        {
+            Pies = pies;
+            CurrentCatergory = currentCatergory;
+        }
+    }
+}
